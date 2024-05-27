b50173becc2f3643cd4693652bd868647c07b3e7
Nimmala Krishna Chaithanya <krishna.nimmala@eizen.ai>
2024-05-27 10:27:52 +0000

    mongo_operations.py edited online with Bitbucket

--- utils/mongo_operations.py
+++ utils/mongo_operations.py
@@ -151,6 +151,7 @@ class MongoDBConnector:
                             #     del message["contextType"]
                             # print(document)
                             message["repetition"]=document['steps'][-1]["repetition"]
+                            print(self.video_instruction_kafka_topic,message)
                             self.producer.send(self.video_instruction_kafka_topic,value=json.dumps(message).encode("utf-8"))
                             # Update status to "completed"
                             if step["status"] != "completed":
