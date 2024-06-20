ea7128c53f6e4abcdf7f8279d6b82a540bc50a92
KC <krishna.nimmala@eizen.ai>
2024-06-20 17:03:30 +0530

    added image to image search

--- core/main.py
+++ core/main.py
@@ -248,6 +248,45 @@ async def chair_detector_action_detector(file, sourceId, sessionId, manualId):
     await detector.chair_action_detector(file=file, sourceId=sourceId, sessionId=sessionId, manualId=manualId)
 
 
+@app.post("/similar_image")
+async def similar_image(input_data: Input):
+    """
+    Endpoint for performing detections.
+
+    Args:
+        input_data (Input): Input data containing file path, sourceId, sessionId, and manualId.
+
+    Returns:
+        dict: Dictionary containing the output of detection.
+    """
+    # Extract input data
+    a=datetime.datetime.now()
+    file = input_data.file
+    sourceId = input_data.sourceId
+    sessionId = input_data.sessionId
+    manualId = input_data.manualId
+    
+    # Perform hand detection
+    asyncio.create_task(similar_image_detector(file=file, sourceId=sourceId, sessionId=sessionId, manualId=manualId))
+
+    # return {"output": []}
+
+async def similar_image_detector(file, sourceId, sessionId, manualId):
+    """
+    Asynchronous function to perform hand detection.
+    
+    Args:
+        file (str): File path.
+        sourceId (str): Source ID.
+        sessionId (str): Session ID.
+        manualId (str): Manual ID.
+    """
+    
+    # Perform hand detection (Replace with your actual implementation)
+    await asyncio.sleep(0)  # Simulate some asynchronous task
+    await detector.get_similar_image_detector(file=file, sourceId=sourceId, sessionId=sessionId, manualId=manualId)
+
+
 
 # Run the FastAPI application
 if __name__ == "__main__":
